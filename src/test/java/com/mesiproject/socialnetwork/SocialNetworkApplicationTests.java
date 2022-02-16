5d0cec75a0ef78d62cf470a79257e9f4244cec2d
Julien <kalaycijulien@gmail.com>
2022-02-16 16:05:44 +0100

    Mise en place du projet

---
+++ src/test/java/com/mesiproject/socialnetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesiproject.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
